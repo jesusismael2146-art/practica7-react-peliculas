45382760dca8ff2235f63522bd1ae3bae3cd2c75
ivanscovarrubias-web <ivan.scovarrubias@alumnos.udg.mx>
2026-04-15 22:05:38 -0600

    Parte 7 y 8 hecha

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "../pages/LandingPage";
+import MovieDetails from "../pages/MovieDetails";
+function AppRouter() {
+return (
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<LandingPage />} />
+<Route path="/movies/:movieId" element={<MovieDetails />} />
+</Routes>
+</BrowserRouter>
+);
+}
+export default AppRouter;
