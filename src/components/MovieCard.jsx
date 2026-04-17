d1a130e11a4ac44e2207bf3405f770927fe8e4ed
ivanscovarrubias-web <ivan.scovarrubias@alumnos.udg.mx>
2026-04-16 21:24:44 -0600

    Parte 14 hecha

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import { getMovieImg } from "../utils/getMovieImg";
+import "./MovieCard.css";
+function MovieCard({ title, posterPath, movieId }) {
+const imageUrl = getMovieImg(posterPath, 500);
+return (
+<li className="movie-card">
+<Link to={`/movies/${movieId}`} className="link-no-underline">
+<img src={imageUrl} alt={title} className="movie-img" />
+<h2 className="movie-title">{title}</h2>
+</Link>
+</li>
+);
+}
+export default MovieCard;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import MovieGrid from "../components/MovieGrid";
+function LandingPage() {
+return (
+<div>
+<MovieGrid />
+</div>
+);
+}
+export default LandingPage;
